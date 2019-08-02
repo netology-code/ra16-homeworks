67dc5b69168ed312d4fe00f308c9ed0b012bd5a2
coursar <coursar@gmail.com>
2019-08-02 14:07:26 +0300

    feat(lifecycle-http): backend for crud added

---
+++ lifecycle-http/crud/backend/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+const notes = [];
+let nextId = 1;
+
+const router = new Router();
+
+router.get('/notes', async (ctx, next) => {
+    ctx.response.body = notes;
+});
+
+router.post('/notes', async(ctx, next) => {
+    notes.push({...ctx.request.body, id: nextId++});
+    ctx.response.status = 204;
+});
+
+router.delete('/notes/:id', async(ctx, next) => {
+    const noteId = Number(ctx.params.id);
+    const index = notes.findIndex(o => o.id === noteId);
+    if (index !== -1) {
+        notes.splice(index, 1);
+    }
+    ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
