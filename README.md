c872f88cebede92949d99c81498c6b2f02a17f8f
coursar <coursar@gmail.com>
2019-08-05 01:08:54 +0300

    feat(router): router homeworks added

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 1. [x] [Жизненный цикл и работа с HTTP](lifecycle-http)
 1. [x] [HOC](hoc)
 1. [x] [hooks, Context API](hooks-context)
-1. [ ] [React Router](router)
+1. [x] [React Router](router)
 1. [ ] [Redux и Redux Thunk](redux)
 1. [ ] [Redux Observable](observable)
 1. [ ] [Redux Saga](saga)
---
+++ router/crud/backend/server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+let posts = [];
+let nextId = 1;
+
+const router = new Router();
+
+router.get('/posts', async (ctx, next) => {
+    ctx.response.body = posts;
+});
+
+router.post('/posts', async(ctx, next) => {
+    const {id, content} = ctx.request.body;
+
+    if (id !== 0) {
+        posts = posts.map(o => o.id !== id ? o : {...o, content: content});
+        ctx.response.status = 204;
+        return;
+    }
+
+    posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
+    ctx.response.status = 204;
+});
+
+router.delete('/posts/:id', async(ctx, next) => {
+    const postId = Number(ctx.params.id);
+    const index = posts.findIndex(o => o.id === postId);
+    if (index !== -1) {
+        posts.splice(index, 1);
+    }
+    ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
