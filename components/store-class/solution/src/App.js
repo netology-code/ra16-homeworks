2e810057d6fc9cc461ac73fa47112583bb5faaa3
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-12-15 23:16:57 +0300

    solution store-class

--- components/store-class/solution/src/App.js
+++ components/store-class/solution/src/App.js
@@ -7,7 +7,7 @@ const item = {
   title: 'Leonard coat',
   description: 'Minimalistic coat in cotton-blend',
   descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
+  price: 800,
   currency: '£'
 }
 
