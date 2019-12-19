4c63582a38966894d987fd79bb3ea4078abe30a4
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-12-19 13:42:41 +0300

    modify layout store

--- events-state/layouts/layouts_solution/src/components/Store.js
+++ events-state/layouts/layouts_solution/src/components/Store.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
+import CardsView from './CardsView'
 
 export class Store extends PureComponent {
     render() {
         return (
             <div>
-                
+                <CardsView />
             </div>
         )
     }
