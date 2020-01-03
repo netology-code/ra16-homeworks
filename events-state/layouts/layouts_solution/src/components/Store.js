c5e0bed4828ce4e88ee765797ffb68668477c310
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-01-03 19:14:37 +0300

    layouts-solution

--- events-state/layouts/layouts_solution/src/components/Store.js
+++ events-state/layouts/layouts_solution/src/components/Store.js
@@ -2,6 +2,10 @@ import React, { PureComponent } from 'react'
 import CardsView from './CardsView'
 
 export class Store extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
     render() {
         return (
             <div>
