99f1b094d178b2fb19972684bc1b05f4f394e277
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-01-05 00:01:25 +0300

    layouts start shop-item

--- events-state/layouts/layouts_solution/src/components/ShopCard.js
+++ events-state/layouts/layouts_solution/src/components/ShopCard.js
@@ -2,20 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function ShopCard(props) {
-    console.log(props)
     return (
-        <div className='card-product'>
-            <h1>
-                {props.product.name}
-            </h1>
-            <span>
-                {props.product.color}
-            </span>
-            <img className='card-image' src={props.product.img} />
-            <span>
-                {props.product.price}
-            </span>
-            <button>Add to card</button>
+        <div className='card card-product'>
+            <div className='card'>
+                <h1>
+                    {props.product.name}
+                </h1>
+                <span>
+                    {props.product.color}
+                </span>
+            </div>
+            <img className='card card-image' src={props.product.img} alt={props.product.name}/>
+            <div className='card'>
+                <span>
+                    {props.product.price}
+                </span>
+                <button>Add to card</button>
+            </div>
         </div>
     )
 }
