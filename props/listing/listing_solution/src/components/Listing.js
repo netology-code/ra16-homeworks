843f4f047ef7c01964710e0db52b4dff223382a7
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-01-11 21:41:23 +0300

    listing_solution completed

--- props/listing/listing_solution/src/components/Listing.js
+++ props/listing/listing_solution/src/components/Listing.js
@@ -3,21 +3,28 @@ import PropTypes from 'prop-types'
 
 function Listing(props) {
 
-    const itemsList = props.items.map(item => (
-        <div className='item'>
-            <div className="item-image">
-              <a href={item.url}>
-                <img src={item.state === 'active' ? item.MainImage.url_570xN : ''} alt={item.title}/>
-              </a>
-            </div>
-            <div className="item-details">
-                {console.log(item.title)}
-                <p className="item-title">{item.title}</p>
-                <p className="item-price">{item.price}</p>
-                <p className={`item-quantity level-${item.quantity <= 10 ? 'low' : item.quantity <= 20 ? 'medium' : 'high'}`}>{item.quantity}</p>
-            </div>
-        </div>
-    ));
+    const itemView = (item) => {
+        
+        if(item.state === 'active') {
+            let shotTitle = Array.from(item.title)
+            shotTitle = shotTitle.length > 50 ? shotTitle.slice(0, 50).join('') + '...' : shotTitle.join('');
+
+            return  <div className='item' key={item.listing_id}>
+                        <div className="item-image">
+                        <a href={item.url}>
+                            <img src={item.state === 'active' ? item.MainImage.url_570xN : ''} alt={item.title}/>
+                        </a>
+                        </div>
+                        <div className="item-details">
+                            <p className="item-title">{shotTitle}</p>
+                            <p className="item-price">{item.price}</p>
+                            <p className={`item-quantity level-${item.quantity <= 10 ? 'low' : item.quantity <= 20 ? 'medium' : 'high'}`}>{item.quantity}</p>
+                        </div>
+                    </div>
+        }
+    }
+
+    const itemsList = props.items.map(item => itemView(item));
 
     return (
         <Fragment>
@@ -28,7 +35,15 @@ function Listing(props) {
 }
 
 Listing.propTypes = {
-
+    items: PropTypes.arrayOf(PropTypes.shape({
+        listing_id: PropTypes.number,
+        url: PropTypes.string,
+        MainImage: PropTypes.object, 
+        title: PropTypes.string,
+        currency_code: PropTypes.string,
+        price: PropTypes.string,
+        quantity: PropTypes.number
+    }))
 }
 
 export default Listing
