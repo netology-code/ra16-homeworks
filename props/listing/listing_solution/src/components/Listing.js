1cc9da5936ac68d2a2b44927a0cb4d11bda46d77
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-01-11 18:42:34 +0300

    listing limit did

--- props/listing/listing_solution/src/components/Listing.js
+++ props/listing/listing_solution/src/components/Listing.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 function Listing(props) {
-    console.log(props.items[0].MainImage.url_570xN);
 
     const itemsList = props.items.map(item => (
-        <div className="item">
+        <div className='item'>
             <div className="item-image">
               <a href={item.url}>
                 <img src={item.state === 'active' ? item.MainImage.url_570xN : ''} alt={item.title}/>
               </a>
             </div>
             <div className="item-details">
+                {console.log(item.title)}
                 <p className="item-title">{item.title}</p>
-                <p className="item-price"></p>
-                <p className="item-quantity level-medium"></p>
+                <p className="item-price">{item.price}</p>
+                <p className={`item-quantity level-${item.quantity <= 10 ? 'low' : item.quantity <= 20 ? 'medium' : 'high'}`}>{item.quantity}</p>
             </div>
         </div>
     ));
 
     return (
-        <div>
+        <Fragment>
             {itemsList}
-        </div>
+        </Fragment>
         
     )
 }
