e2ed9dc4bf37cb6ad21a52b2dcf8d43e277293c2
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-03-19 15:18:27 +0300

    final version

--- forms/steps/steps_solution/src/components/WrapForm.js
+++ forms/steps/steps_solution/src/components/WrapForm.js
@@ -57,30 +57,6 @@ function WrapForm(props) {
             </div>
             </form>
             <List listDays={days} onRemove={handleRemove}/>
-            {/* <div className='list-wrap'>
-                <div>
-                    <p className='header-info'>Дата (ДД.ММ.ГГ)</p>
-                    <p className='header-info'>Пройдено км</p>
-                    <p className='header-info'>Действия</p>
-                </div>
-                <ul className='days-list'>
-                    {days
-                        .sort((a, b) => {
-                            const date = new Date(a.date.split('.').reverse().join('-'));
-                            const date2 = new Date(b.date.split('.').reverse().join('-'));
-                            return date - date2
-                        })
-                        .reverse()
-                        .map( o => 
-                            <li className='day-info' key={o.id}>
-                                <p className='day-data'>{o.date}</p>
-                                <p className='day-data'>{o.distance}</p>
-                                <button className='day-data delete-btn' onClick={() => handleRemove(o.id)}>✘</button>
-                            </li>
-                        )
-                    }
-                </ul>
-            </div> */}
         </React.Fragment>
     )
 }
