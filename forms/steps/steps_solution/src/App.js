3ff449e3cdb682cc20855188f7f787d4f3a872cd
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-03-23 17:01:35 +0300

    final steps

--- forms/steps/steps_solution/src/App.js
+++ forms/steps/steps_solution/src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './App.css';
+import {useState} from 'react';
+import DayModel from './components/DayModel';
+import nanoid from 'nanoid';
 import WrapForm from './components/WrapForm';
+import List from './components/List';
 
 function App() {
+  const baseForm = {
+    date: '',
+    distance: ''
+  };
+  const [form, setForm] = useState({
+      date: '',
+      distance: ''
+  });
+  const [days, setDays] = useState([]);
+
+  const handleChange = evt => {
+      const {name, value} = evt.target;
+      setForm(prevForm => ({...prevForm, [name]: value}));
+  }
+
+  const handleForm = evt => {
+      evt.preventDefault();
+      const checkDay = days.filter(day => day.date === form.date);
+
+      if(checkDay.length > 0) {
+          const day = new DayModel(nanoid(), checkDay[0].date, `${+form.distance + +checkDay[0].distance}`);
+          handleRemove(checkDay[0].id);
+          setDays(prevDays => [...prevDays, day]);
+      } else {
+          const day = new DayModel(nanoid(), form.date, form.distance);
+          setDays(prevDays => [...prevDays, day]);
+      }
+
+      setForm(baseForm);
+  }
+
+  const handleRemove = id => {
+      setDays(prevDays => prevDays.filter(day => day.id !== id))
+  }
+
   return (
     <div className="App">
-      <WrapForm />
+      <WrapForm date={form.date} distance={form.distance} onSubmit={handleForm} onChange={handleChange}/>
+      <List listDays={days} onRemove={handleRemove}/>
     </div>
   );
 }
--- forms/steps/steps_solution/src/components/WrapForm.js
+++ forms/steps/steps_solution/src/components/WrapForm.js
@@ -1,63 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {useState} from 'react'
-import nanoid from 'nanoid'
-import DayModel from './DayModel'
-import List from './List'
 
 function WrapForm(props) {
-    const baseForm = {
-        date: '',
-        distance: ''
-    };
-    const [form, setForm] = useState({
-        date: '',
-        distance: ''
-    });
-    const [days, setDays] = useState([]);
 
-    const handleChange = evt => {
-        const {name, value} = evt.target;
-        setForm(prevForm => ({...prevForm, [name]: value}));
+    const date=props.date;
+    const distance= props.distance;
+    
+    const handleChange = (evt) => {
+        return props.onChange(evt)
     }
 
-    const handleForm = evt => {
-        evt.preventDefault();
-        const checkDay = days.filter(day => day.date === form.date);
-        
-        if(checkDay.length > 0) {
-            const day = new DayModel(nanoid(), checkDay[0].date, `${+form.distance + +checkDay[0].distance}`);
-            handleRemove(checkDay[0].id);
-            setDays(prevDays => [...prevDays, day]);
-        } else {
-            const day = new DayModel(nanoid(), form.date, form.distance);
-            setDays(prevDays => [...prevDays, day]);
-        }
-
-        setForm(baseForm);
-    }
-
-    const handleRemove = id => {
-        setDays(prevDays => prevDays.filter(day => day.id !== id))
+    const handleForm = (evt) => {
+        return props.onSubmit(evt)
     }
 
     return (
-        <React.Fragment>
-            <form className='input-form' onSubmit={handleForm}>
-            <div className='input-wrap'>
-                <div className='day'>
-                    <label htmlFor='name'>Дата (ДД. ММ. ГГ.)</label>
-                    <input name='date' value={form.date} onChange={handleChange}/>
-                </div>
-                <div className='day'>
-                    <label htmlFor='name'>Пройдено км</label>
-                    <input name='distance' value={form.distance} onChange={handleChange}/>
-                </div>
-                <button className='day submit-btn'> OK </button>
+        <form className='input-form' onSubmit={handleForm}>
+        <div className='input-wrap'>
+            <div className='day'>
+                <label htmlFor='name'>Дата (ДД. ММ. ГГ.)</label>
+                <input name='date' value={date} onChange={handleChange}/>
             </div>
-            </form>
-            <List listDays={days} onRemove={handleRemove}/>
-        </React.Fragment>
+            <div className='day'>
+                <label htmlFor='name'>Пройдено км</label>
+                <input name='distance' value={distance} onChange={handleChange}/>
+            </div>
+            <button className='day submit-btn'> OK </button>
+        </div>
+        </form>
     )
 }
 
