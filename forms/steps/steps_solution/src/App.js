e6f09f0690e8c0bc946c7a9f02d683bf174abf0b
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-03-24 17:09:15 +0300

    new repair version

--- forms/steps/steps_solution/src/App.js
+++ forms/steps/steps_solution/src/App.js
@@ -1,41 +1,15 @@
 import React from 'react';
 import './App.css';
 import {useState} from 'react';
-import DayModel from './components/DayModel';
-import nanoid from 'nanoid';
 import WrapForm from './components/WrapForm';
 import List from './components/List';
 
 function App() {
-  const baseForm = {
-    date: '',
-    distance: ''
-  };
-  const [form, setForm] = useState({
-      date: '',
-      distance: ''
-  });
+  
   const [days, setDays] = useState([]);
 
-  const handleChange = evt => {
-      const {name, value} = evt.target;
-      setForm(prevForm => ({...prevForm, [name]: value}));
-  }
-
-  const handleForm = evt => {
-      evt.preventDefault();
-      const checkDay = days.filter(day => day.date === form.date);
-
-      if(checkDay.length > 0) {
-          const day = new DayModel(nanoid(), checkDay[0].date, `${+form.distance + +checkDay[0].distance}`);
-          handleRemove(checkDay[0].id);
-          setDays(prevDays => [...prevDays, day]);
-      } else {
-          const day = new DayModel(nanoid(), form.date, form.distance);
-          setDays(prevDays => [...prevDays, day]);
-      }
-
-      setForm(baseForm);
+  const handleSetDaysList = (day) => {
+    setDays(prevDays => [...prevDays, day]);
   }
 
   const handleRemove = id => {
@@ -44,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <WrapForm date={form.date} distance={form.distance} onSubmit={handleForm} onChange={handleChange}/>
+      <WrapForm listDays={days} onRemove={handleRemove} onSubmit={handleSetDaysList}/>
       <List listDays={days} onRemove={handleRemove}/>
     </div>
   );
--- forms/steps/steps_solution/src/components/WrapForm.js
+++ forms/steps/steps_solution/src/components/WrapForm.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {useState} from 'react';
+import DayModel from './DayModel';
+import nanoid from 'nanoid';
 
 function WrapForm(props) {
-
-    const date=props.date;
-    const distance= props.distance;
+    const baseForm = {
+        date: '',
+        distance: ''
+    };
+    const [form, setForm] = useState({
+        date: '',
+        distance: ''
+    });
     
-    const handleChange = (evt) => {
-        return props.onChange(evt)
+    const days = props.listDays;
+
+    const handleRemove = (id) => {
+        return props.onRemove(id)
     }
 
-    const handleForm = (evt) => {
-        return props.onSubmit(evt)
+    const handleSetDaysList = (day) => {
+        return props.onSubmit(day)
+    }
+
+    const handleChange = evt => {
+        const {name, value} = evt.target;
+        setForm(prevForm => ({...prevForm, [name]: value}));
+    }
+  
+    const handleForm = evt => {
+        evt.preventDefault();
+        const checkDay = days.filter(day => day.date === form.date);
+  
+        if(checkDay.length > 0) {
+            const day = new DayModel(nanoid(), checkDay[0].date, `${+form.distance + +checkDay[0].distance}`);
+            handleRemove(checkDay[0].id);
+            handleSetDaysList(day);
+        } else {
+            const day = new DayModel(nanoid(), form.date, form.distance);
+            handleSetDaysList(day);
+        }
+  
+        setForm(baseForm);
     }
 
     return (
@@ -19,11 +50,11 @@ function WrapForm(props) {
         <div className='input-wrap'>
             <div className='day'>
                 <label htmlFor='name'>Дата (ДД. ММ. ГГ.)</label>
-                <input name='date' value={date} onChange={handleChange}/>
+                <input name='date' value={form.date} onChange={handleChange}/>
             </div>
             <div className='day'>
                 <label htmlFor='name'>Пройдено км</label>
-                <input name='distance' value={distance} onChange={handleChange}/>
+                <input name='distance' value={form.distance} onChange={handleChange}/>
             </div>
             <button className='day submit-btn'> OK </button>
         </div>
