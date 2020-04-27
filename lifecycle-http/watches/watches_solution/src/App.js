5efc564d190c3c388892e7c579073dd2fb2be1e7
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-04-27 12:36:53 +0300

    problem with setIterval

--- lifecycle-http/watches/watches_solution/src/App.js
+++ lifecycle-http/watches/watches_solution/src/App.js
@@ -1,31 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import AddWatch from './components/AddWatch';
 import WatchTable from './components/WatchTable';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {watches:[]};
-    
-    this.handleClick = this.handleClick.bind(this)
+function App() {
+
+  const [watches, setWatches] = useState([])
+
+  const  handleClick = (data) => {
+      setWatches(watch => [...watch, data])
   }
 
-  handleClick(data) {
-    this.setState(state => {
-      state.watches.push(data)
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <AddWatch handleClick={this.handleClick}/>
-        <WatchTable allWatches={this.state.watches}/>
-        {console.log(this.state.watches)}
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <AddWatch handleClick={handleClick}/>
+      <WatchTable allWatches={watches}/>
+    </div>
+  )
 }
 
 export default App;
--- lifecycle-http/watches/watches_solution/src/components/WatchTable.js
+++ lifecycle-http/watches/watches_solution/src/components/WatchTable.js
@@ -3,16 +3,30 @@ import React from 'react';
 class WatchTable extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            count: 0,
+            currentWatches: this.props.allWatches
+        }
     }
 
     render() {
+    // const currentWatches = this.props.allWatches.map(el => <li>{el.name}</li>)
         return(
             <div>
-               <ul>
-                    
-               </ul>
+                <ul>
+                    {this.state.currentWatches.map(el => <li>{el.name}</li>)}
+                </ul>
+                <p>{this.state.count}</p>
             </div>
         )
+    }
+
+    componentDidMount() {
+    //    this.interval = setInterval(() => {   
+    //     this.setState(state => {
+    //         state.count = state.count + 1
+    //     })
+    //    }, 1000)
     }
 }
 
