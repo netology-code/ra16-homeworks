5e677f4bc665c1e068a1f42b1b6f0bbba4ca30ce
pevec1 <pevec1@yandex.ru>
2023-07-11 00:02:14 +0300

    exe 1

---
+++ components/store-func-ex/src/App.js
@@ -0,0 +1,27 @@
+import ShopItemFunc from './ShopItemFunc';
+import './App.css';
+
+function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'      
+}
+return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  )
+}
+
+export default App;
