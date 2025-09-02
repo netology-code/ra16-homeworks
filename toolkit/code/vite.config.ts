dcf67e58b4b072c8a7f58c21d6beba80dde03013
Rysaev Damir <freepad@ya.ru>
2025-09-02 21:47:51 +0300

    init rtk code

---
+++ toolkit/code/vite.config.ts
@@ -0,0 +1,28 @@
+import react from "@vitejs/plugin-react"
+import * as path from "node:path"
+import { defineConfig } from "vitest/config"
+import packageJson from "./package.json" with { type: "json" }
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    open: true,
+  },
+
+  test: {
+    root: import.meta.dirname,
+    name: packageJson.name,
+    environment: "jsdom",
+
+    typecheck: {
+      enabled: true,
+      tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
+    },
+
+    globals: true,
+    watch: false,
+    setupFiles: ["./src/setupTests.ts"],
+  },
+})
